d3c70660b31f42996d02be409448e8da547f80c4
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-10 06:21:10 +0530

    Update start.py

--- DAXXMUSIC/plugins/bot/start.py
+++ DAXXMUSIC/plugins/bot/start.py
@@ -28,14 +28,25 @@ from strings import get_string
 
 
 YUMI_PICS = [
-"https://graph.org/file/f86b71018196c5cfe7344.jpg",
-"https://graph.org/file/a3db9af88f25bb1b99325.jpg",
-"https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
-"https://graph.org/file/84de4b440300297a8ecb3.jpg",
-"https://graph.org/file/84e84ff778b045879d24f.jpg",
-"https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
-"https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
-"https://graph.org/file/d6360613d0fa7a9d2f90b.jpg"
+"https://telegra.ph/file/7611ad355da9de11423e8.jpg",
+"https://telegra.ph/file/1166532656cd26238c94b.jpg",
+"https://telegra.ph/file/57c386813a1a26746479d.jpg",
+"https://telegra.ph/file/c41810d3f632921d00b43.jpg",
+"https://telegra.ph/file/b912059691f481b8b3439.jpg",
+"https://telegra.ph/file/5aad0a2c595547cfbd59a.jpg",
+"https://telegra.ph/file/a8402f4a91a42893a7928.jpg",
+"https://telegra.ph/file/717dcd9ac67965556bc9f.jpg",
+"https://telegra.ph/file/5e339c35608aedc2c6e86.jpg",
+"https://telegra.ph/file/cb16f4f1f141f88fd9dc3.jpg",
+"https://telegra.ph/file/aaadeab176227f51c0d55.jpg",
+"https://telegra.ph/file/993bfbb0f4c5302b1f65d.jpg",
+"https://telegra.ph/file/729e77a7871fe0075d695.jpg",
+"https://telegra.ph/file/460e59ccd14e203f69166.jpg",
+"https://telegra.ph/file/c706584f9c4d8ec4120c7.jpg",
+"https://telegra.ph/file/ded86839c47bdd843109b.jpg",
+"https://telegra.ph/file/4bbdd86219a0d996c6bbf.jpg",
+"https://telegra.ph/file/3d34c6a50a50d48e0090a.jpg",
+"https://telegra.ph/file/46f3c54fc68bb142e57b7.jpg",
 
 ]
 
