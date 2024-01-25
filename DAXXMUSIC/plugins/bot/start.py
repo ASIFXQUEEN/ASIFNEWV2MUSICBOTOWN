4d7e76397426904bd47ae9a1246847844ffcfd66
RAJ RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2024-01-25 18:05:18 +0530

    Update start.py

--- DAXXMUSIC/plugins/bot/start.py
+++ DAXXMUSIC/plugins/bot/start.py
@@ -116,7 +116,7 @@ async def start_pm(client, message: Message, _):
         served_chats = len(await get_served_chats())	
         served_users = len(await get_served_users())	
         UP, CPU, RAM, DISK = await bot_sys_stats()	
-        await message.reply_sticker("CAACAgIAAxkBAAIFN2WyTFRbUAJe50AE40U3bMIS7Gj-AAIyAANkYXEufJi7Pr5eNa4eBA"),
+        await message.reply_sticker("CAACAgUAAxkBAAIFcmWyVVaCQiP4cUAqkf8TzWEHWx0RAALQBAAC7tYJVAg-LNcyFtq7HgQ"),
         await asyncio.sleep(0.2)
         await acha.delete()
         await message.reply_photo(	
