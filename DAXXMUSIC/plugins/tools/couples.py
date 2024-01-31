59bdcf2afb3b3ea9fc0d49e86baffeaca4946300
RAJ RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2024-01-31 17:08:33 +0530

    Update couples.py

--- DAXXMUSIC/plugins/tools/couples.py
+++ DAXXMUSIC/plugins/tools/couples.py
@@ -56,8 +56,8 @@ async def ctest(_, message):
          photo1 = (await app.get_chat(6785082234)).photo
          photo2 = (await app.get_chat(6632587591)).photo
  
-         N1 = (await app.get_users 6785082234).mention 
-         N2 = (await app.get_users 6632587591).mention
+         N1 = (await app.get_users(6785082234)).mention 
+         N2 = (await app.get_users(6632587591)).mention
          
          try:
             p1 = await app.download_media(photo1.big_file_id, file_name="pfp.png")
