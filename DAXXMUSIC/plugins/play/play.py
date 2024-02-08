c9b3b05f6c5138df91bd53dc7bc84ead64d4870d
RAJ RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2024-02-08 20:35:30 +0530

    Update play.py

--- DAXXMUSIC/plugins/play/play.py
+++ DAXXMUSIC/plugins/play/play.py
@@ -56,7 +56,7 @@ async def play_commnd(
                     ]
                 ]
             ),
-        )
+  ):
         return
     mystic = await message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
